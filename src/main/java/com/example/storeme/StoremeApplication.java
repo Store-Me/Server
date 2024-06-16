92e27354a2a5e81ee95f00dbc86cea6ec2f9211d
hyeokson <cksgur1765@konkuk.ac.kr>
2024-06-17 02:04:52 +0900

    first commit

---
+++ src/main/java/com/example/storeme/StoremeApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremeApplication.class, args);
+	}
+
+}
