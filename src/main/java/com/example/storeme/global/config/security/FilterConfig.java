0ef4e3fd6a97348b5edc7f6d50a6e44bd05ef1aa
hyeokson <cksgur1765@konkuk.ac.kr>
2024-09-28 00:21:48 +0900

    feat: JWT 인증 필터 등록 설정

---
+++ src/main/java/com/example/storeme/global/config/security/FilterConfig.java
@@ -0,0 +1,16 @@
+package com.example.storeme.global.config.security;
+
+import com.example.storeme.global.filter.JwtAuthenticationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter> registration(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        FilterRegistrationBean<JwtAuthenticationFilter> registration = new FilterRegistrationBean<>(jwtAuthenticationFilter);
+        registration.setEnabled(false);
+        return registration;
+    }
+}
