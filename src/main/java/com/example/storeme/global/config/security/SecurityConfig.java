e4f5697fb25cfaa333e44ff93a556069e14558ce
hyeokson <cksgur1765@konkuk.ac.kr>
2024-09-28 00:26:53 +0900

    feat: spring security 설정 클래스 구현

---
+++ src/main/java/com/example/storeme/global/config/security/SecurityConfig.java
@@ -0,0 +1,87 @@
+package com.example.storeme.global.config.security;
+
+import com.example.storeme.global.filter.JwtAccessDeniedHandler;
+import com.example.storeme.global.filter.JwtAuthenticationEntryPoint;
+import com.example.storeme.global.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .and()
+                .ignoring()
+                .requestMatchers("/swagger-ui/**")
+                .requestMatchers("/swagger","/v3/**")
+                .requestMatchers("/error/**")
+                .requestMatchers(HttpMethod.GET, "/health");
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .cors(AbstractHttpConfigurer::disable)
+
+                .formLogin(AbstractHttpConfigurer::disable)
+
+                .httpBasic(AbstractHttpConfigurer::disable)
+
+                .logout(LogoutConfigurer::disable)
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"/user", "/login", "/logout").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/user",
+                                "/comment/{feedId}/public",
+                                "/feed/{feedId}/public", "/feed/list/public","/feed/search/public",
+                                "/feed/user/public",
+                                "code/*",
+                                "follow/follower", "follow/followed",
+                                "/notice/**").permitAll()
+                        .requestMatchers("/userId/check","/mail").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/notice/{noticeId}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/notice/{noticeId}").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+
+                .exceptionHandling((exceptionHandlingConfigurer)->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler))
+
+
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
