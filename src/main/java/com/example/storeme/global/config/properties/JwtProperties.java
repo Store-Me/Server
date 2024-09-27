ada7859620e6ffd1da18faff16ce24ce660ff048
hyeokson <cksgur1765@konkuk.ac.kr>
2024-09-28 00:24:17 +0900

    feat: JWT 속성 클래스 생성

---
+++ src/main/java/com/example/storeme/global/config/properties/JwtProperties.java
@@ -0,0 +1,27 @@
+package com.example.storeme.global.config.properties;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+@Getter
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private final String bearer;
+    private final String secret;
+    private final String accessHeader;
+    private final String refreshHeader;
+    private final Long accessExpiration;
+    private final Long refreshExpiration;
+
+    @ConstructorBinding
+    public JwtProperties(String bearer, String secret, String accessHeader, String refreshHeader,
+                         Long accessExpiration, Long refreshExpiration){
+        this.bearer = bearer;
+        this.secret = secret;
+        this.accessHeader = accessHeader;
+        this.refreshHeader = refreshHeader;
+        this.accessExpiration = accessExpiration;
+        this.refreshExpiration = refreshExpiration;
+    }
+}
