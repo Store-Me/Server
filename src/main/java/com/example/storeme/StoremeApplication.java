82a45fca85cab11ebc811e773291b8a8317746de
hyeokson <cksgur1765@konkuk.ac.kr>
2024-09-28 00:18:51 +0900

    feat: 애노테이션 추가

--- src/main/java/com/example/storeme/StoremeApplication.java
+++ src/main/java/com/example/storeme/StoremeApplication.java
@@ -2,8 +2,12 @@ package com.example.storeme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class StoremeApplication {
 
 	public static void main(String[] args) {
