d4d67b02d8408fea5e7a8669844241267de06ec5
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:36:35 +0900

    refactor: 변수명 변경

--- src/main/java/com/example/storeme/global/common/exception/JwtAuthenticationException.java
+++ src/main/java/com/example/storeme/global/common/exception/JwtAuthenticationException.java
@@ -4,12 +4,15 @@ import com.example.storeme.global.common.code.BaseErrorCode;
 import lombok.Getter;
 import org.springframework.security.core.AuthenticationException;
 
+/**
+ * JWT 인증 예외 클래스
+ */
 @Getter
 public class JwtAuthenticationException extends AuthenticationException {
-    private final BaseErrorCode code;
+    private final BaseErrorCode errorCode;
 
-    public JwtAuthenticationException(BaseErrorCode code){
-        super(code.getErrorMsg());
-        this.code=code;
+    public JwtAuthenticationException(BaseErrorCode errorCode){
+        super(errorCode.getErrorMsg());
+        this.errorCode=errorCode;
     }
 }
