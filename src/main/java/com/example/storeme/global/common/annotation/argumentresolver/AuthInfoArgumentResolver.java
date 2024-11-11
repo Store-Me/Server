f6a0457d9c4afbd2a7529362df15aadcee624d2a
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:28:44 +0900

    refactor: user entity의 id값을 바인딩하도록 변경

--- src/main/java/com/example/storeme/global/common/annotation/argumentresolver/AuthInfoArgumentResolver.java
+++ src/main/java/com/example/storeme/global/common/annotation/argumentresolver/AuthInfoArgumentResolver.java
@@ -1,5 +1,7 @@
-package com.example.storeme.global.config.web.argumentresolver;
+package com.example.storeme.global.common.annotation.argumentresolver;
 
+import com.example.storeme.fo_domain.user.domain.User;
+import com.example.storeme.global.common.annotation.AuthInfo;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.NonNull;
 import org.springframework.core.MethodParameter;
@@ -8,16 +10,19 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-public class UserInfoArgumentResolver implements HandlerMethodArgumentResolver {
+/**
+ * 인증에 성공한 유저의 id값을 반환해주는 Argument Resolver 클래스
+ */
+public class AuthInfoArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameterAnnotation(UserId.class) !=null
+        return parameter.getParameterAnnotation(AuthInfo.class) !=null
                 && parameter.getParameterType().equals(Long.class);
     }
 
     @Override
     public Object resolveArgument(@NonNull MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory)  {
         HttpServletRequest req = (HttpServletRequest) webRequest.getNativeRequest();
-        return req.getAttribute("UserInfo");
+        return req.getAttribute("userId");
     }
 }
