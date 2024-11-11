6668b17177fc8fc59c0a98f379795587643111f0
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:46:25 +0900

    feat: 메서드 추가
    - 전화번호로 User객체를 조회하는 메서드
    - 계정Id에 해당하는 User가 있는지 확인하는 메서드

--- src/main/java/com/example/storeme/fo_domain/user/repository/UserRepository.java
+++ src/main/java/com/example/storeme/fo_domain/user/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.example.storeme.fo_domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    boolean existsByAccountId(String accountId);
 }
