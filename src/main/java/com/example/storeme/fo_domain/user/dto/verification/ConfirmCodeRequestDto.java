8bab761c00d3a68c1c6a875e95c3564729fa11bc
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:18:12 +0900

    feat: 사용자 인증 코드 확인 요청 Dto 클래스 생성

---
+++ src/main/java/com/example/storeme/fo_domain/user/dto/verification/ConfirmCodeRequestDto.java
@@ -0,0 +1,27 @@
+package com.example.storeme.fo_domain.user.dto.verification;
+
+import com.example.storeme.fo_domain.user.constant.SignupType;
+import com.example.storeme.global.common.constant.ValidationConstant;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 사용자 인증 코드 확인 요청 Dto 클래스
+ */
+@NoArgsConstructor
+@Getter
+public class ConfirmCodeRequestDto {
+
+    @Pattern(regexp = ValidationConstant.PHONE_NUMBER_REGEX,
+            message = ValidationConstant.PHONE_NUMBER_MSG)
+    private String phoneNumber;
+
+    @Pattern(regexp = ValidationConstant.VERIFICATION_CODE_REGEX,
+            message = ValidationConstant.VERIFICATION_CODE_MSG)
+    private String verificationCode;
+
+    @NotNull
+    private SignupType signupType;
+}
