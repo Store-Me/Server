77575cd8ebfed351ff0bb95e4a107cd25712a499
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:24:52 +0900

    feat: 사용자와 관련된 예외를 처리하는 클래스 생성

---
+++ src/main/java/com/example/storeme/fo_domain/user/exception/UserException.java
@@ -0,0 +1,14 @@
+package com.example.storeme.fo_domain.user.exception;
+
+import com.example.storeme.global.common.code.BaseErrorCode;
+import com.example.storeme.global.common.exception.GeneralException;
+
+/**
+ * 사용자와 관련된 예외를 처리하는 클래스
+ */
+public class UserException extends GeneralException {
+
+    public UserException(BaseErrorCode code) {
+        super(code);
+    }
+}
