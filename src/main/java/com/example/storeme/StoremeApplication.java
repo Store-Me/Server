f7bce11e27b373008f7b412c7b3b5d5c410f699f
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:49:47 +0900

    feat: EntityListener 애노테이션 추가

--- src/main/java/com/example/storeme/StoremeApplication.java
+++ src/main/java/com/example/storeme/StoremeApplication.java
@@ -1,13 +1,16 @@
 package com.example.storeme;
 
+import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
+@EntityListeners(AuditingEntityListener.class)
 public class StoremeApplication {
 
 	public static void main(String[] args) {
