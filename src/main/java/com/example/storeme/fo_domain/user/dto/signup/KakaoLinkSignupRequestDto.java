fc3e8c19f253258537bb24f5e9a889f946e47efa
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:19:37 +0900

    feat: App 계정 연동 회원가입 요청 Dto 클래스 생성

---
+++ src/main/java/com/example/storeme/fo_domain/user/dto/signup/KakaoLinkSignupRequestDto.java
@@ -0,0 +1,28 @@
+package com.example.storeme.fo_domain.user.dto.signup;
+
+import com.example.storeme.global.common.constant.ValidationConstant;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * App 계정 연동 회원가입 요청 Dto 클래스
+ */
+@Getter
+@NoArgsConstructor
+public class KakaoLinkSignupRequestDto {
+
+    @NotNull
+    @Pattern(regexp = ValidationConstant.KAKAO_ID_REGEX, message = ValidationConstant.KAKAO_ID_MSG)
+    private String kakaoId;
+
+    @NotNull
+    @Pattern(regexp = ValidationConstant.PHONE_NUMBER_REGEX,
+            message = ValidationConstant.PHONE_NUMBER_MSG)
+    private String phoneNumber;
+
+    @Pattern(regexp = ValidationConstant.VERIFICATION_CODE_REGEX,
+            message = ValidationConstant.VERIFICATION_CODE_MSG)
+    private String verificationCode;
+}
