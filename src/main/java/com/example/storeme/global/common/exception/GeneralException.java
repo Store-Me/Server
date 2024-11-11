d373576b9ea112e38d94e4760f398f9ee91af3b8
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:34:06 +0900

    refactor: error code만 반환하도록 변경

--- src/main/java/com/example/storeme/global/common/exception/GeneralException.java
+++ src/main/java/com/example/storeme/global/common/exception/GeneralException.java
@@ -1,21 +1,16 @@
 package com.example.storeme.global.common.exception;
 
 import com.example.storeme.global.common.code.BaseErrorCode;
-import com.example.storeme.global.common.response.ResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * 커스텀 예외의 최고 조상 클래스
+ */
 @Getter
 @AllArgsConstructor
 public class GeneralException extends RuntimeException {
 
-    private final BaseErrorCode code;
+    private final BaseErrorCode errorCode;
 
-    public ResponseDto.ErrorReasonDto getErrorReason() {
-        return this.code.getReason();
-    }
-
-    public ResponseDto.ErrorReasonDto getErrorReasonHttpStatus() {
-        return this.code.getReasonHttpStatus();
-    }
 }
