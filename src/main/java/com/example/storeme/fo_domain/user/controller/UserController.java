9273fa68fd15872da0a0927838f35b99a185c66e
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-11 23:24:27 +0900

    feat: 유저 관련 요청을 처리하는 컨트롤러 클래스 생성

--- src/main/java/com/example/storeme/fo_domain/user/controller/UserController.java
+++ src/main/java/com/example/storeme/fo_domain/user/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.storeme.fo_domain.user.controller;
 
+import com.example.storeme.fo_domain.user.service.UserService;
+import com.example.storeme.global.common.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 유저 관련 요청을 처리하는 컨트롤러 클래스
+ */
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+
 }
