4ce67a0f321213a073f63565cce26f0a33a5441c
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-12 21:22:26 +0900

    feat: 로그인 컨트롤러 메서드 구현

--- src/main/java/com/example/storeme/fo_domain/user/controller/UserController.java
+++ src/main/java/com/example/storeme/fo_domain/user/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.example.storeme.fo_domain.user.controller;
 
+import com.example.storeme.fo_domain.user.dto.user.AppLoginRequestDto;
+import com.example.storeme.fo_domain.user.dto.user.JwtResponseDto;
+import com.example.storeme.fo_domain.user.dto.user.KakaoLoginRequestDto;
 import com.example.storeme.fo_domain.user.service.UserService;
 import com.example.storeme.global.common.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,9 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "유저 관련 요청 처리")
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/login/app")
+    @Operation(summary = "App 계정 로그인")
+    public ResponseDto<JwtResponseDto> handleAppLogin(@Valid @RequestBody AppLoginRequestDto appLoginRequestDto){
+        return ResponseDto.onSuccess(userService.handleAppLogin(appLoginRequestDto));
+    }
+
+    @PostMapping("/login/kakao")
+    @Operation(summary = "Kakao 계정 로그인")
+    public ResponseDto<JwtResponseDto> handleAppLogin(@Valid @RequestBody KakaoLoginRequestDto kakaoLoginRequestDto){
+        return ResponseDto.onSuccess(userService.handleKakaoLogin(kakaoLoginRequestDto));
+    }
 
 
 }
