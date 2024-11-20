c40c9cb2f4d118d37b298cb7b6630fc9e4e35685
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-20 23:22:15 +0900

    feat: S3 폴더를 관리하는 enum 클래스 구현

---
+++ src/main/java/com/example/storeme/global/config/s3/constant/S3Folder.java
@@ -0,0 +1,21 @@
+package com.example.storeme.global.config.s3.constant;
+
+import lombok.Getter;
+
+/**
+ * S3 폴더를 관리하는 enum 클래스
+ */
+@Getter
+public enum S3Folder {
+    CUSTOMER_PROFILE_IMAGE("customer-profile-image/"),
+    STORE_PROFILE_IMAGE("store-profile-image/"),
+    STORE_IMAGE("store-image/"),
+    MENU_IMAGE("menu-image/"),
+    COUPON_IMAGE("coupon-image/");
+
+    private final String path;
+
+    S3Folder(String path) {
+        this.path = path;
+    }
+}
