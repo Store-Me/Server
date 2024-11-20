3e6ea1f481c0410efcb54db17f6ba48b3f52fe71
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-20 23:23:39 +0900

    feat: 가게 카테고리 enum 클래스 구현

---
+++ src/main/java/com/example/storeme/fo_domain/user/constant/StoreCategory.java
@@ -0,0 +1,13 @@
+package com.example.storeme.fo_domain.user.constant;
+
+/**
+ * 가게 카테고리 enum 클래스
+ */
+public enum StoreCategory {
+    RESTAURANT,
+    CAFE,
+    BEAUTY,
+    MEDICAL,
+    EXERCISE,
+    SALON
+}
