63a2d5b898e6ac83d2ffc4a65ace6451383fa83d
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-20 23:10:44 +0900

    refactor: 유저 권한 정보 수정

--- src/main/java/com/example/storeme/fo_domain/user/constant/RoleType.java
+++ src/main/java/com/example/storeme/fo_domain/user/constant/RoleType.java
@@ -1,18 +1,13 @@
 package com.example.storeme.fo_domain.user.constant;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 /**
  * 유저의 권한 정보
  */
-@Getter
-@RequiredArgsConstructor
 public enum RoleType {
 
-    ROLE_USER("USER"), // 일반 유저
-    ROLE_ADMIN("ADMIN"); // 관리자 유저
+    CUSTOMER, // 손님 유저
+    OWNER, // 사장님 유저
+    DUAL, // 손님 & 사장님 둘다 가능한 유저
+    ADMIN; // 관리자 유저
 
-    private final String roleType;
 }
