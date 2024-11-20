ce95761df8a2c41fdbd87e99b2423b977b8f37df
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-20 23:10:11 +0900

    refactor: 클래스명 변경

--- src/main/java/com/example/storeme/fo_domain/user/dto/signup/KakaoCustomerSignupRequestDto.java
+++ src/main/java/com/example/storeme/fo_domain/user/dto/signup/KakaoCustomerSignupRequestDto.java
@@ -3,19 +3,16 @@ package com.example.storeme.fo_domain.user.dto.signup;
 import com.example.storeme.global.common.constant.ValidationConstant;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 /**
- * Kakao 계정 회원가입 요청 Dto 클래스
+ * Kakao 계정 손님타입 회원가입 요청 Dto 클래스
  */
 @Getter
 @NoArgsConstructor
-public class KakaoSignupRequestDto {
+public class KakaoCustomerSignupRequestDto {
 
     @NotNull
     @Pattern(regexp = ValidationConstant.KAKAO_ID_REGEX, message = ValidationConstant.KAKAO_ID_MSG)
