9ac3f0f76f92bd5bd5371fca43c4e01ce37c1086
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-20 23:27:53 +0900

    feat: 사장님 정보 수정 요청 Dto 클래스 구현

---
+++ src/main/java/com/example/storeme/fo_domain/user/dto/user/UpdateOwnerInfoRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.storeme.fo_domain.user.dto.user;
+
+import com.example.storeme.global.common.constant.ValidationConstant;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 사장님 정보 수정 요청 Dto 클래스
+ */
+@NoArgsConstructor
+@Getter
+public class UpdateOwnerInfoRequestDto {
+
+    @Pattern(regexp = ValidationConstant.ACCOUNT_ID_REGEX, message = ValidationConstant.ACCOUNT_ID_MSG)
+    private Long storeId;
+
+    @Pattern(regexp = ValidationConstant.ACCOUNT_ID_REGEX, message = ValidationConstant.ACCOUNT_ID_MSG)
+    private String accountId;
+
+    @Pattern(regexp = ValidationConstant.PASSWORD_REGEX, message = ValidationConstant.PASSWORD_MSG)
+    private String password;
+
+}
