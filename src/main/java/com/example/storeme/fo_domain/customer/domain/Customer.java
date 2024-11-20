d9317679f2d07472b18461901fff3cb5b1e2bcf2
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-20 23:18:36 +0900

    feat: 손님 도메인 클래스 구현

---
+++ src/main/java/com/example/storeme/fo_domain/customer/domain/Customer.java
@@ -0,0 +1,29 @@
+package com.example.storeme.fo_domain.customer.domain;
+
+import com.example.storeme.fo_domain.store.domain.Store;
+import com.example.storeme.fo_domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "customer_infos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "customer")
+    private User user;
+
+    @Column(name = "nickname", nullable = false, length = 30)
+    private String nickname;
+
+    @Column(name = "profile_image_url", length = 2048)
+    private String profileImageUrl;
+}
