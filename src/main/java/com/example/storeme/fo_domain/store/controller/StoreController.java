a0c093c41c3ea0471707dbf15bd5d154601f0550
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-28 16:21:07 +0900

    feat: 가게 컨트롤러 및 서비스 클래스 구현

---
+++ src/main/java/com/example/storeme/fo_domain/store/controller/StoreController.java
@@ -0,0 +1,110 @@
+package com.example.storeme.fo_domain.store.controller;
+
+import com.example.storeme.fo_domain.store.dto.SaveStoreInfoRequestDto;
+import com.example.storeme.fo_domain.store.dto.StoreInfoListResponseDto;
+import com.example.storeme.fo_domain.store.dto.StoreInfoResponseDto;
+import com.example.storeme.fo_domain.store.dto.UpdateStoreInfoRequestDto;
+import com.example.storeme.fo_domain.store.service.StoreService;
+import com.example.storeme.global.common.annotation.AuthInfo;
+import com.example.storeme.global.common.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Encoding;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+/**
+ * 가게 정보 요청을 처리하는 컨트롤러 클래스
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/store")
+@Tag(name = "가게 관련 요청 처리")
+public class StoreController {
+
+    private final StoreService storeService;
+
+    /**
+     * 유저가 관리하는 가게 정보 리스트 조회 요청을 처리하는 메서드
+     */
+    @Operation(summary = "유저의 가게 정보 리스트 조회")
+    @GetMapping("/list")
+    public ResponseDto<StoreInfoListResponseDto> getStoreInfoList(@Parameter(hidden = true) @AuthInfo Long userId) {
+
+        StoreInfoListResponseDto storeInfoListResponseDto = storeService.getStoreInfoList(userId);
+        return ResponseDto.onSuccess(storeInfoListResponseDto);
+    }
+
+    /**
+     * 가게 정보 조회 요청을 처리하는 메서드
+     */
+    @Operation(summary = "가게 정보 조회")
+    @GetMapping
+    public ResponseDto<StoreInfoResponseDto> getStoreInfo(@Parameter(hidden = true) @AuthInfo Long userId,
+                                                          @NotNull @RequestParam Long storeId) {
+
+        StoreInfoResponseDto storeInfoResponseDto = storeService.getStoreInfo(userId, storeId);
+        return ResponseDto.onSuccess(storeInfoResponseDto);
+    }
+
+    /**
+     * 가게 정보 저장 요청을 처리하는 메서드
+     */
+    @Operation(summary = "가게 정보 저장")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseDto<Void> saveStoreInfo(@Parameter(hidden = true) @AuthInfo Long userId,
+            @RequestPart("saveStoreInfoRequestDto")
+            @Parameter(
+                    content = @Content(
+                            encoding = @Encoding(name = "saveStoreInfoRequestDto",
+                                    contentType = MediaType.APPLICATION_JSON_VALUE)))
+            @Valid SaveStoreInfoRequestDto saveStoreInfoRequestDto,
+            @RequestPart(value = "storeProfileImageFile", required = false) MultipartFile storeProfileImageFile,
+            @RequestPart(value = "storeFeaturedImageFile", required = false) MultipartFile storeFeaturedImageFile,
+            @RequestPart(value = "storeImageFileList", required = false) List<MultipartFile> storeImageFileList,
+            @RequestPart(value = "storeBannerImage", required = false) MultipartFile storeBannerImageFile) {
+
+        storeService.saveStoreInfo(userId, saveStoreInfoRequestDto, storeProfileImageFile,
+                storeFeaturedImageFile, storeImageFileList, storeBannerImageFile);
+        return ResponseDto.onSuccess();
+    }
+
+    /**
+     * 가게 정보 수정 요청을 처리하는 메서드
+     */
+    @Operation(summary = "가게 정보 수정")
+    @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseDto<Void> updateStoreInfo(@Parameter(hidden = true) @AuthInfo Long userId,
+            @RequestPart("updateStoreInfoRequestDto")
+            @Parameter(
+                    content = @Content(
+                            encoding = @Encoding(name = "updateStoreInfoRequestDto",
+                                    contentType = MediaType.APPLICATION_JSON_VALUE)))
+            @Valid UpdateStoreInfoRequestDto updateStoreInfoRequestDto,
+            @RequestPart(value = "storeProfileImageFile", required = false) MultipartFile storeProfileImageFile,
+            @RequestPart(value = "storeBannerImageFile", required = false) MultipartFile storeBannerImageFile) {
+
+        storeService.updateStoreInfo(userId, updateStoreInfoRequestDto, storeProfileImageFile, storeBannerImageFile);
+        return ResponseDto.onSuccess();
+    }
+
+    /**
+     * 가게 정보 삭제 요청을 처리하는 메서드
+     */
+    @Operation(summary = "가게 정보 삭제")
+    @DeleteMapping
+    public ResponseDto<Void> deleteStoreInfo(@Parameter(hidden = true) @AuthInfo Long userId,
+                                             @NotNull @RequestParam Long storeId) {
+
+        storeService.deleteStoreInfo(userId, storeId);
+        return ResponseDto.onSuccess();
+    }
+}
---
+++ src/main/java/com/example/storeme/fo_domain/store/service/StoreService.java
@@ -0,0 +1,247 @@
+package com.example.storeme.fo_domain.store.service;
+
+import com.example.storeme.fo_domain.store.domain.Store;
+import com.example.storeme.fo_domain.store.dto.SaveStoreInfoRequestDto;
+import com.example.storeme.fo_domain.store.dto.StoreInfoListResponseDto;
+import com.example.storeme.fo_domain.store.dto.StoreInfoResponseDto;
+import com.example.storeme.fo_domain.store.dto.UpdateStoreInfoRequestDto;
+import com.example.storeme.fo_domain.store.exception.StoreException;
+import com.example.storeme.fo_domain.store.repository.StoreRepository;
+import com.example.storeme.fo_domain.storeimage.domain.StoreImage;
+import com.example.storeme.fo_domain.storeimage.repository.StoreImageRepository;
+import com.example.storeme.fo_domain.user.domain.User;
+import com.example.storeme.fo_domain.user.exception.UserException;
+import com.example.storeme.fo_domain.user.repository.UserRepository;
+import com.example.storeme.global.common.code.status.ErrorStatus;
+import com.example.storeme.global.config.s3.constant.S3Folder;
+import com.example.storeme.global.config.s3.service.ImageFileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+
+    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
+    private final StoreImageRepository storeImageRepository;
+    private final ImageFileService imageFileService;
+
+    /**
+     * 유저가 관리하는 가게 정보 리스트를 반환하는 메서드
+     */
+    @Transactional(readOnly = true)
+    public StoreInfoListResponseDto getStoreInfoList(Long userId){
+
+        return StoreInfoListResponseDto.builder()
+                .storeInfoList(storeRepository.findStoreInfoByUserId(userId))
+                .build();
+    }
+
+    /**
+     * 가게 정보를 반환하는 메서드
+     */
+    @Transactional(readOnly = true)
+    public StoreInfoResponseDto getStoreInfo(Long userId, Long storeId){
+
+        if(!storeRepository.existsByIdAndUserId(storeId, userId)){
+            log.error("The store is not for the user");
+            throw new StoreException(ErrorStatus._BAD_REQUEST);
+        }
+
+        Store store = storeRepository.findById(storeId).orElseThrow(()->{
+            log.error("Store not found with id: {}", storeId);
+            return new StoreException(ErrorStatus._BAD_REQUEST);
+        });
+
+        return StoreInfoResponseDto.builder()
+                .storeName(store.getName())
+                .storeProfileImageUrl(store.getProfileImageUrl())
+                .storeFeaturedImageUrl(store.getFeaturedImageUrl())
+                .storeImageDtoList(store.getStoreImageList().stream()
+                        .map((storeImage)-> StoreInfoResponseDto.StoreImageDto.builder()
+                                .storeImageId(storeImage.getId())
+                                .storeImageUrl(storeImage.getImageUrl())
+                                .order(storeImage.getOrder())
+                                .build())
+                        .sorted(Comparator.comparingInt(StoreInfoResponseDto.StoreImageDto::getOrder))
+                        .toList())
+                .storeBannerImageUrl(store.getBannerImageUrl())
+                .storeDescription(store.getDescription())
+                .storeCategory(store.getCategory())
+                .storeDetailCategory(store.getDetailCategory())
+                .storeLocation(store.getLocation())
+                .storeLocationCode(store.getLocationCode())
+                .storeLocationDetail(store.getLocationDetail())
+                .storeLat(store.getLat())
+                .storeLng(store.getLng())
+                .storePhoneNumber(store.getPhoneNumber())
+                .storeNotice(store.getNotice())
+                .storeIntro(store.getIntro())
+                .build();
+    }
+
+    /**
+     * 가게 정보를 저장하는 메서드
+     */
+    @Transactional
+    public void saveStoreInfo(Long userId, SaveStoreInfoRequestDto saveStoreInfoRequestDto,
+                              MultipartFile storeProfileImageFile,
+                              MultipartFile storeFeaturedImageFile,
+                              List<MultipartFile> storeImageFileList,
+                              MultipartFile storeBannerImageFile){
+
+        User user = userRepository.findById(userId).orElseThrow(()->{
+            log.error("User not found with id: {}", userId);
+            return new UserException(ErrorStatus._BAD_REQUEST);
+        });
+
+        String storeProfileImageFileUrl = imageFileService.uploadImageFile(S3Folder.STORE_PROFILE_IMAGE, storeProfileImageFile);
+        String storeFeaturedImageFileUrl = imageFileService.uploadImageFile(S3Folder.STORE_IMAGE, storeFeaturedImageFile);
+        List<StoreImage> storeImageFileUrlList = IntStream.range(0, storeImageFileList.size())
+                .mapToObj(index -> StoreImage.builder()
+                        .imageUrl(imageFileService.uploadImageFileList(S3Folder.STORE_IMAGE, storeImageFileList).get(index))
+                        .order(index)
+                        .build())
+                .toList();
+        String storeBannerImageFileUrl = imageFileService.uploadImageFile(S3Folder.STORE_BANNER_IMAGE, storeBannerImageFile);
+
+        Store store = Store.builder()
+                .name(saveStoreInfoRequestDto.getStoreName())
+                .profileImageUrl(storeProfileImageFileUrl)
+                .featuredImageUrl(storeFeaturedImageFileUrl)
+                .bannerImageUrl(storeBannerImageFileUrl)
+                .description(saveStoreInfoRequestDto.getStoreDescription())
+                .category(saveStoreInfoRequestDto.getStoreCategory())
+                .detailCategory(saveStoreInfoRequestDto.getStoreDetailCategory())
+                .location(saveStoreInfoRequestDto.getStoreLocation())
+                .locationCode(saveStoreInfoRequestDto.getStoreLocationCode())
+                .locationDetail(saveStoreInfoRequestDto.getStoreLocationDetail())
+                .lat(saveStoreInfoRequestDto.getStoreLat())
+                .lng(saveStoreInfoRequestDto.getStoreLng())
+                .phoneNumber(saveStoreInfoRequestDto.getStorePhoneNumber())
+                .intro(saveStoreInfoRequestDto.getStoreIntro())
+                .notice(saveStoreInfoRequestDto.getStoreNotice())
+                .build();
+
+        store.addStoreImageList(storeImageFileUrlList);
+        user.addStore(store);
+    }
+
+    /**
+     * 가게 정보를 수정하는 메서드
+     */
+    @Transactional
+    public void updateStoreInfo(Long userId, UpdateStoreInfoRequestDto updateStoreInfoRequestDto,
+                                MultipartFile storeProfileImageFile,
+                                MultipartFile storeBannerImageFile){
+
+        if(!storeRepository.existsByIdAndUserId(updateStoreInfoRequestDto.getStoreId(), userId)){
+            log.error("The store is not for the user");
+            throw new StoreException(ErrorStatus._BAD_REQUEST);
+        }
+
+        Store store = storeRepository.findById(updateStoreInfoRequestDto.getStoreId()).orElseThrow(()->{
+            log.error("Store not found with id: {}", updateStoreInfoRequestDto.getStoreId());
+            return new StoreException(ErrorStatus._BAD_REQUEST);
+        });
+
+        if(storeProfileImageFile != null && storeProfileImageFile.isEmpty())
+            store.setProfileImageUrl(null);
+        else if(storeProfileImageFile != null){
+            String storeProfileImageFileUrl = imageFileService.uploadImageFile(
+                    S3Folder.STORE_PROFILE_IMAGE, storeProfileImageFile);
+            imageFileService.deleteImageFile(store.getProfileImageUrl());
+
+            store.setProfileImageUrl(storeProfileImageFileUrl);
+        }
+
+        if(storeBannerImageFile != null && storeBannerImageFile.isEmpty())
+            store.setBannerImageUrl(null);
+        else if(storeBannerImageFile != null){
+            String storeBannerImageFileUrl = imageFileService.uploadImageFile(
+                    S3Folder.STORE_BANNER_IMAGE, storeBannerImageFile
+            );
+            imageFileService.deleteImageFile(store.getBannerImageUrl());
+
+            store.setBannerImageUrl(storeBannerImageFileUrl);
+        }
+
+
+        if(updateStoreInfoRequestDto.getStoreName().isPresent())
+            store.setName(updateStoreInfoRequestDto.getStoreName().get());
+
+        if(updateStoreInfoRequestDto.getStoreDescription().isPresent())
+            store.setDescription(updateStoreInfoRequestDto.getStoreDescription().get());
+
+        if(updateStoreInfoRequestDto.getStoreCategory().isPresent())
+            store.setCategory(updateStoreInfoRequestDto.getStoreCategory().get());
+
+        if(updateStoreInfoRequestDto.getStoreDetailCategory().isPresent())
+            store.setDetailCategory(updateStoreInfoRequestDto.getStoreDetailCategory().get());
+
+        if(updateStoreInfoRequestDto.getStoreFeaturedImageId().isPresent()){
+            if(updateStoreInfoRequestDto.getStoreFeaturedImageId().get() == null)
+                store.setFeaturedImageUrl(null);
+            else
+                store.setFeaturedImageUrl(
+                        storeImageRepository.findById(updateStoreInfoRequestDto.getStoreFeaturedImageId().get())
+                                .orElseThrow(()->{
+                                    log.error("Store image not found for id: {}", updateStoreInfoRequestDto.getStoreFeaturedImageId().get());
+                                    return new StoreException(ErrorStatus._BAD_REQUEST);
+                                }).getImageUrl());
+        }
+
+        if(updateStoreInfoRequestDto.getStoreLocation().isPresent())
+            store.setLocation(updateStoreInfoRequestDto.getStoreLocation().get());
+
+        if(updateStoreInfoRequestDto.getStoreLocationCode().isPresent())
+            store.setLocationCode(updateStoreInfoRequestDto.getStoreLocationCode().get());
+
+        if(updateStoreInfoRequestDto.getStoreLocationDetail().isPresent())
+            store.setLocationDetail(updateStoreInfoRequestDto.getStoreLocationDetail().get());
+
+        if(updateStoreInfoRequestDto.getStoreLat().isPresent())
+            store.setLat(updateStoreInfoRequestDto.getStoreLat().get());
+
+        if(updateStoreInfoRequestDto.getStoreLng().isPresent())
+            store.setLng(updateStoreInfoRequestDto.getStoreLng().get());
+
+        if(updateStoreInfoRequestDto.getStorePhoneNumber().isPresent())
+            store.setPhoneNumber(updateStoreInfoRequestDto.getStorePhoneNumber().get());
+
+        if(updateStoreInfoRequestDto.getStoreIntro().isPresent())
+            store.setIntro(updateStoreInfoRequestDto.getStoreIntro().get());
+
+        if(updateStoreInfoRequestDto.getStoreNotice().isPresent())
+            store.setNotice(updateStoreInfoRequestDto.getStoreNotice().get());
+
+    }
+
+    /**
+     * 가게 정보를 삭제하는 메서드
+     */
+    @Transactional
+    public void deleteStoreInfo(Long userId, Long storeId){
+
+        if(!storeRepository.existsById(storeId)){
+            log.error("Store not found with id: {}", storeId);
+            throw new StoreException(ErrorStatus._BAD_REQUEST);
+        }
+
+        if(!storeRepository.existsByIdAndUserId(storeId, userId)){
+            log.error("The store is not for the user");
+            throw new StoreException(ErrorStatus._BAD_REQUEST);
+        }
+
+        storeRepository.deleteById(storeId);
+    }
+}
