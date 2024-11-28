5ad8f16ae541655cd093d26a0dec03f3b1bb29a6
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-28 16:21:35 +0900

    feat: 가게 이미지 순서 수정 요청 Dto 클래스 구현

---
+++ src/main/java/com/example/storeme/fo_domain/storeimage/dto/UpdateStoreImageOrderRequestDto.java
@@ -0,0 +1,30 @@
+package com.example.storeme.fo_domain.storeimage.dto;
+
+import com.example.storeme.fo_domain.storeimage.annotation.ValidOrder;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 가게 이미지 순서 수정 요청 Dto 클래스
+ */
+@Getter
+@NoArgsConstructor
+public class UpdateStoreImageOrderRequestDto {
+
+    @NotNull
+    private Long storeId;
+
+    @ValidOrder
+    List<StoreImageOrderInfoDto> storeImageOrderInfoList;
+
+    @Getter
+    public static class StoreImageOrderInfoDto{
+        @NotNull
+        private Long storeImageId;
+        @NotNull
+        private Integer storeImageOrder;
+    }
+}
