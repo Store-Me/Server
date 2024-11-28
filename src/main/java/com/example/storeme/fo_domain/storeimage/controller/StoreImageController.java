51bc09468ecde2d24ee5f1f16261640cd2ec211a
hyeokson <cksgur1765@konkuk.ac.kr>
2024-11-28 16:20:42 +0900

    feat: 가게 이미지 컨트롤러 및 서비스 클래스 구현

---
+++ src/main/java/com/example/storeme/fo_domain/storeimage/controller/StoreImageController.java
@@ -0,0 +1,67 @@
+package com.example.storeme.fo_domain.storeimage.controller;
+
+import com.example.storeme.fo_domain.storeimage.dto.UpdateStoreImageOrderRequestDto;
+import com.example.storeme.fo_domain.storeimage.service.StoreImageService;
+import com.example.storeme.global.common.annotation.AuthInfo;
+import com.example.storeme.global.common.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+/**
+ * 가게 정보 요청을 처리하는 컨트롤러 클래스
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/store/image")
+@Tag(name = "가게 이미지 관련 요청 처리")
+public class StoreImageController {
+    private final StoreImageService storeImageService;
+
+    /**
+     * 가게 이미지 저장 요청을 처리하는 메서드
+     */
+    @Operation(summary = "가게 이미지 저장")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseDto<Void> saveStoreImage(@Parameter(hidden = true) @AuthInfo Long userId,
+                                           @NotNull @RequestParam Long storeId,
+                                           @RequestPart(value = "storeImageFileList", required = false)
+                                           List<MultipartFile> storeImageFileList) {
+
+        storeImageService.saveStoreImageFile(userId, storeId, storeImageFileList);
+        return ResponseDto.onSuccess();
+    }
+
+    /**
+     * 가게 이미지 삭제 요청을 처리하는 메서드
+     */
+    @Operation(summary = "가게 이미지 삭제")
+    @DeleteMapping
+    public ResponseDto<Void> deleteStoreImage(@Parameter(hidden = true) @AuthInfo Long userId,
+                                              @NotNull @RequestParam Long storeId,
+                                              @NotNull @RequestParam Long storeImageId) {
+
+        storeImageService.deleteStoreImageFile(userId, storeId, storeImageId);
+        return ResponseDto.onSuccess();
+    }
+
+    /**
+     * 가게 이미지 순서 값 수정 요청을 처리하는 메서드
+     */
+    @Operation(summary = "가게 이미지 순서 수정")
+    @PatchMapping
+    public ResponseDto<Void> updateStoreImageOrder(@Parameter(hidden = true) @AuthInfo Long userId,
+                                                   @Valid @RequestBody
+                                                   UpdateStoreImageOrderRequestDto updateStoreImageOrderRequestDto){
+        storeImageService.updateStoreImageOrder(userId, updateStoreImageOrderRequestDto);
+        return ResponseDto.onSuccess();
+    }
+}
---
+++ src/main/java/com/example/storeme/fo_domain/storeimage/service/StoreImageService.java
@@ -0,0 +1,107 @@
+package com.example.storeme.fo_domain.storeimage.service;
+
+import com.example.storeme.fo_domain.store.domain.Store;
+import com.example.storeme.fo_domain.store.exception.StoreException;
+import com.example.storeme.fo_domain.store.repository.StoreRepository;
+import com.example.storeme.fo_domain.storeimage.domain.StoreImage;
+import com.example.storeme.fo_domain.storeimage.dto.UpdateStoreImageOrderRequestDto;
+import com.example.storeme.fo_domain.storeimage.exception.StoreImageException;
+import com.example.storeme.fo_domain.storeimage.repository.StoreImageRepository;
+import com.example.storeme.global.common.code.status.ErrorStatus;
+import com.example.storeme.global.config.s3.constant.S3Folder;
+import com.example.storeme.global.config.s3.service.ImageFileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StoreImageService {
+
+    private final StoreRepository storeRepository;
+    private final StoreImageRepository storeImageRepository;
+    private final ImageFileService imageFileService;
+
+    /**
+     * 가게 사진을 저장하는 메서드
+     */
+    @Transactional
+    public void saveStoreImageFile(Long userId, Long storeId, List<MultipartFile> storeImageFileList){
+        if(!storeRepository.existsByIdAndUserId(storeId, userId)){
+            log.error("The store is not for the user");
+            throw new StoreImageException(ErrorStatus._BAD_REQUEST);
+        }
+
+        Store store = storeRepository.findById(storeId).orElseThrow(()->{
+            log.error("Store not found with id: {}", storeId);
+            return new StoreImageException(ErrorStatus._BAD_REQUEST);
+        });
+
+        List<String> storeImageFileUrlList = imageFileService.uploadImageFileList(
+                S3Folder.STORE_IMAGE,
+                storeImageFileList);
+
+        store.getStoreImageList().addAll(IntStream.range(0, storeImageFileUrlList.size())
+                .mapToObj(index -> StoreImage.builder()
+                        .imageUrl(storeImageFileUrlList.get(index))
+                        .order(store.getStoreImageList().size() + index)
+                        .build())
+                .toList());
+    }
+
+    /**
+     * 가게 사진을 삭제하는 메서드
+     */
+    @Transactional
+    public void deleteStoreImageFile(Long userId, Long storeId, Long storeImageId){
+        if(!storeRepository.existsByIdAndUserId(storeId, userId)){
+            log.error("The store is not for the user");
+            throw new StoreImageException(ErrorStatus._BAD_REQUEST);
+        }
+
+        if(!storeImageRepository.existsByIdAndStoreId(storeImageId, storeId)){
+            log.error("The storeImage is not for the store");
+            throw new StoreImageException(ErrorStatus._BAD_REQUEST);
+        }
+
+        storeImageRepository.deleteById(storeImageId);
+    }
+
+    /**
+     * 가게 사진 순서값을 수정하는 메서드
+     */
+    @Transactional
+    public void updateStoreImageOrder(Long userId,
+                                      UpdateStoreImageOrderRequestDto updateStoreImageOrderRequestDto){
+
+        Long storeId = updateStoreImageOrderRequestDto.getStoreId();
+
+        if(!storeRepository.existsByIdAndUserId(storeId, userId)){
+            log.error("The store is not for the user");
+            throw new StoreImageException(ErrorStatus._BAD_REQUEST);
+        }
+
+        updateStoreImageOrderRequestDto.getStoreImageOrderInfoList()
+                .forEach(storeImageOrderInfoDto -> {
+                    Long storeImageId = storeImageOrderInfoDto.getStoreImageId();
+
+                    if(!storeImageRepository.existsByIdAndStoreId(storeImageId, storeId)){
+                        log.error("The storeImage is not for the store");
+                        throw new StoreImageException(ErrorStatus._BAD_REQUEST);
+                    }
+
+                    StoreImage storeImage = storeImageRepository.findById(storeImageId).orElseThrow(()->{
+                        log.error("Store image not found with id: {}", storeImageId);
+                        return new StoreImageException(ErrorStatus._BAD_REQUEST);
+                    });
+
+                    storeImage.setOrder(storeImage.getOrder());
+                });
+    }
+}
